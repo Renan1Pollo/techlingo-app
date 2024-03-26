91af7effe08c8d47cd0849dcabddd6dc198ebba8
Renan Pollo Benelli <renansk18@outlook.com>
2024-03-26 13:29:23 -0300

    initial commit
    Feat: importacao do bootstrap, criacao de um componente a adicao de uma pagina de login

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarMenuComponent } from "./components/sidebar-menu/sidebar-menu.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, SidebarMenuComponent]
+})
+export class AppComponent {
+  title = 'techlingo-app';
+}
