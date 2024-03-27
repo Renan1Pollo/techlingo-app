8c391e69b6e3b031831f5dc0424153834e921513
Renan Pollo Benelli <renansk18@outlook.com>
2024-03-26 22:11:19 -0300

    Criação do componente de Login
    Feat: Separação da tela de login em um componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarMenuComponent } from "./components/sidebar-menu/sidebar-menu.component";
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, SidebarMenuComponent]
+    imports: [RouterOutlet, SidebarMenuComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'techlingo-app';
