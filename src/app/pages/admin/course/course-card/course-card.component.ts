b83ebf578436663f3decb29ca845947a1881d448
Renan Pollo Benelli <renansk18@outlook.com>
2024-08-01 22:27:39 -0300

    feat: add course card component

---
+++ src/app/pages/admin/course/course-card/course-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../../../../types/Course.type';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-course-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.scss'
+})
+export class CourseCardComponent {
+  @Input() course!: Course;
+}
