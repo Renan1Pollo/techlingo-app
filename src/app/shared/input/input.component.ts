163a9f8ba6c70c808b0b05047f633de9942ea020
RenanPollo <renansk18@outlook.com>
2024-09-25 12:09:44 -0300

    feat: add mask on input component

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,17 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
+
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule,NgxMaskDirective, NgxMaskPipe],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+  styleUrl: './input.component.scss',
 })
 export class InputComponent implements OnInit, OnChanges, DoCheck {
   @Input() form!: FormGroup;
@@ -20,8 +18,13 @@ export class InputComponent implements OnInit, OnChanges, DoCheck {
   @Input() type: string = 'text';
   @Input() placeholder: string = '';
   @Input() currency: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() maxlength: any;
+  @Input() minlength: any;
+  @Input() asteriskRequired: boolean = true;
+  @Input() mask: string = '';
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes) {
@@ -31,5 +34,5 @@ export class InputComponent implements OnInit, OnChanges, DoCheck {
     }
   }
 
-  ngDoCheck(): void { }
+  ngDoCheck(): void {}
 }
