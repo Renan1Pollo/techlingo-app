79a9d374f120f07b90af310830fbc315a1d62ed2
Renan Pollo Benelli <renansk18@outlook.com>
2024-10-01 20:47:29 -0300

    fixed user-profile component

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,28 +1,68 @@
+import { CommonModule, DatePipe, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 import { Component, OnInit } from '@angular/core';
+import { catchError, tap, throwError } from 'rxjs';
 import { SidebarMenuComponent } from "../../components/sidebar-menu/sidebar-menu.component";
+import { CourseResponseDTO } from '../../types/Course.type';
+import { EnrollmentResponseDTO } from '../../types/Enrollment.type';
+import { User } from '../../types/User.type';
+import { EnrollmentService } from './../../services/enrollment.service';
+
+registerLocaleData(localePt);
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [SidebarMenuComponent],
+  imports: [SidebarMenuComponent, CommonModule],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+  styleUrls: ['./user-profile.component.scss'],
+  providers: [DatePipe]
 })
 export class UserProfileComponent implements OnInit {
-  userProfile: any;
+  user!: User;
+  courses: CourseResponseDTO[] = [];
 
-  constructor() { }
+  constructor(
+    private enrollmentService: EnrollmentService,
+    private datePipe: DatePipe
+  ) { }
 
   ngOnInit(): void {
-    // Dados fictícios, substitua pelos dados reais que vêm do seu serviço ou API
-    this.userProfile = {
-      username: 'JohnDoe',
-      createdAt: new Date(2022, 5, 15), // Data de criação do usuário
-      courses: [
-        { name: 'Curso de Angular', imageUrl: '' },
-        { name: 'Curso de TypeScript', imageUrl: '' },
-        { name: 'Curso de UX Design', imageUrl: '' }
-      ]
+    this.loadUserProfile();
+    this.loadUserEnrollments();
+  }
+
+  private loadUserProfile(): void {
+    this.user = this.getUserFromLocalStorage();
+  }
+
+  private getUserFromLocalStorage(): User {
+    const userData = localStorage.getItem('user');
+    return userData ? JSON.parse(userData) : {} as User;
+  }
+
+  private loadUserEnrollments(): void {
+    this.enrollmentService.getEnrollmentsByUserId(this.user.id)
+      .pipe(
+        tap(enrollments => this.extractCourses(enrollments)),
+        catchError(this.handleError('Error fetching enrollments'))
+      )
+      .subscribe();
+  }
+
+  private extractCourses(enrollments: EnrollmentResponseDTO[]): void {
+    this.courses = enrollments.map(enrollment => enrollment.course);
+  }
+
+  private handleError(message: string) {
+    return (error: any) => {
+      console.error(message, error);
+      return throwError(() => new Error(message));
     };
+  }
+
+  getFormattedCreationDate(): string {
+    const monthYear = this.datePipe.transform(this.user.creationDate, 'MMMM yyyy', 'UTC', 'pt');
+    return monthYear ? monthYear.replace(' ', ' de ') : '';
   }
 }
